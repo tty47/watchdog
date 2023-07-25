d276c5ec562a079e3fe771e03e7d2192c247654a
Jose Ramon Mañes <jose@celestia.org>
2023-07-25 12:57:55 +0200

    docs: add comments to the code
    Signed-off-by: Jose Ramon Mañes <jose@celestia.org>

--- metrics.go
+++ metrics.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 
 	"go.opentelemetry.io/otel"
@@ -10,17 +9,21 @@ import (
 	"go.opentelemetry.io/otel/metric"
 )
 
+// Get the meter from the global meter provider with the name "watchdog".
 var meter = otel.GetMeterProvider().Meter("watchdog")
 
+// LoadBalancer represents the information for a load balancer.
 type LoadBalancer struct {
-	ServiceName      string
-	LoadBalancerName string
-	LoadBalancerIP   string
-	Value            float64
-	Namespace        string
+	ServiceName      string  // ServiceName Name of the service associated with the load balancer.
+	LoadBalancerName string  // LoadBalancerName Name of the load balancer.
+	LoadBalancerIP   string  // LoadBalancerIP IP address of the load balancer.
+	Namespace        string  // Namespace where the service is deployed.
+	Value            float64 // Value to be observed for the load balancer.
 }
 
+// WithMetricsLoadBalancer creates a callback function to observe metrics for multiple load balancers.
 func WithMetricsLoadBalancer(loadBalancers []LoadBalancer) error {
+	// Create a Float64ObservableGauge named "load_balancer" with a description for the metric.
 	loadBalancersGauge, err := meter.Float64ObservableGauge(
 		"load_balancer",
 		metric.WithDescription("Service WatchDog - Load Balancers"),
@@ -30,22 +33,24 @@ func WithMetricsLoadBalancer(loadBalancers []LoadBalancer) error {
 		return err
 	}
 
+	// Define the callback function that will be called periodically to observe metrics.
 	callback := func(ctx context.Context, observer metric.Observer) error {
 		for _, lb := range loadBalancers {
+			// Create labels with attributes for each load balancer.
 			labels := metric.WithAttributes(
 				attribute.String("service_name", lb.ServiceName),
 				attribute.String("load_balancer_name", lb.LoadBalancerName),
 				attribute.String("load_balancer_ip", lb.LoadBalancerIP),
 				attribute.String("namespace", lb.Namespace),
 			)
+			// Observe the float64 value for the current load balancer with the associated labels.
 			observer.ObserveFloat64(loadBalancersGauge, lb.Value, labels)
 		}
 
 		return nil
 	}
 
+	// Register the callback with the meter and the Float64ObservableGauge.
 	_, err = meter.RegisterCallback(callback, loadBalancersGauge)
-	fmt.Println("-------------------------------------------")
-
 	return err
 }
